46e58fa6a4e08ff320830ddc80e995e6d7927aba
MuratYucedag <yucedagmurat23@gmail.com>
2023-04-14 17:24:35 +0300

    Add project files.

---
+++ Edukator.DtoLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Edukator.DtoLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ Edukator.EntityLayer/Concrete/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.EntityLayer.Concrete
+{
+   public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+        public int Review { get; set; }
+    }
+}
